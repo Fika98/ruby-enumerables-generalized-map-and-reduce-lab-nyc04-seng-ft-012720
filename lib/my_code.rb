8137764ff02db748478e976ca8e6debcd0c8da30
Firuz Afzalov <afzalov98@gmail.com>
2020-01-21 21:44:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(array)
+  new = []
+  counter = 0 
+  while counter < array.length 
+  new.push(yield(array[counter]))
+  counter += 1
+end 
+  new
+end 
+
+def reduce(array)
+  total = 0
+  counter = 0 
+  while counter < array.length
+  yield(total += array[counter])
+  counter += 1 
+end 
+total 
+end 
+  
